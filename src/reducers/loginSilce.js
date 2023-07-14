799f109c1a4ba741cc88c83a4bf10985c490e163
sj <sujung033131@gmail.com>
2023-07-14 17:38:54 +0900

    0714 React Redux_login(cookie)

--- src/reducers/loginSilce.js
+++ src/reducers/loginSilce.js
@@ -1,4 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { getCookie, setCookie } from "../util/cookieUtil";
+
+const loadCookie = () => {
+
+    const loginObj = getCookie("login")
+
+    console.log("login...........obj.....")
+    console.log(loginObj)
+
+    if(!loginObj){
+        return initState
+    }
+
+    return loginObj
+
+}
 
 const initState = {
     email:'',
@@ -7,13 +23,16 @@ const initState = {
 
 const loginSilce = createSlice({
     name: 'loginSlice',
-    initialState: initState,
+    initialState: loadCookie(),
     reducers: {
         requestLogin: (state, param) => {
             const payload = param.payload
             console.log("requestLogin", payload)
+            const loginObj = {email: payload.email, signed:true}
 
-            return {email: payload.email, signed:true}
+            setCookie("login", JSON.stringify(loginObj), 1)
+
+            return loginObj
         }
     }
 })
