93307044e00849b4e44abfa8391dc6e888422e55
sj <sujung033131@gmail.com>
2023-07-20 12:01:55 +0900

    0720 JWT accessToken, refreshToken

--- src/components/products/ListComponent.js
+++ src/components/products/ListComponent.js
@@ -25,6 +25,11 @@ const ListComponent = ({ queryObj, movePage, moveRead }) => {
     getList(queryObj).then(data => {
       console.log(data)
       setListData(data)
+    }).catch(err => {
+
+      console.log("----------------------------")
+      console.log(err)
+      console.log("----------------------------")
     })
 
     // queryObj 안에 저장되어있는 친구들중에 하나라도 바뀌면 바뀌게끔
